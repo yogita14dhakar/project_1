ec877277f02ef902f45ad38af7283a7050170305
Yogita Dhakar <144700589+yogita14dhakar@users.noreply.github.com>
2024-11-15 19:44:47 +0530

    Update listings.js

--- controllers/listings.js
+++ controllers/listings.js
@@ -4,8 +4,9 @@ const mapkey = process.env.MAP_KEY;
 
 //to show all listings on one page
 module.exports.index = async (req, res) => {
-    let {country} = req.query.loc;
-    const allListings = (!country) ? await Listing.find({}) : await Listing.find({ $or: [{country: "/^" + country + "/i"}, {location: "/^" + country + "/i"}]});
+    let country = req.query.loc;
+    const allListings = (country === "undefined" || coun
+                        === null) ? await Listing.find({}) : await Listing.find({ $or: [{country: "/^" + country + "/i"}, {location: "/^" + country + "/i"}]});
     if(!allListings){
         req.flash("error", `Listing in ${country} does not exist!`)
         res.redirect("/listings");
