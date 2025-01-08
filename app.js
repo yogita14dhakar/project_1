ab1b99e4257ba2200843ac242ad7e730d13c62d0
Yogita Dhakar <144700589+yogita14dhakar@users.noreply.github.com>
2025-01-08 22:59:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use("/listings", listingRouter);
+app.use("/", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
